a937bd8e96fc6b745b69432990fddf652d077e9d
Mouhamadou SY <mouhamadou@Host-001.lan>
2024-12-09 21:55:02 +0100

    Menu item implementation

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { montserrat } from "../layout";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
 
 export default function DashboardLayout({
     children,
@@ -9,14 +11,18 @@ export default function DashboardLayout({
   }>) {
     return <div className="h-screen flex">
         {/*LEFT */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
           <Link href="/" className="flex items-center justify-center gap-1">
             <Image className="object-contain" src="/logog.png" alt="logo" width={32} height={32}/>
             <span className={`${montserrat.className} hidden lg:block font-medium`}>GSchool</span>
           </Link>
+          <Menu/>
           </div>
         {/*RIGHT */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar />
+            {children}
+        </div>
     </div>;
   }
 
