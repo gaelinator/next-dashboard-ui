ea839e6e29bc0a27257d7fb0450ba1fa9ffb364c
Mouhamadou SY <mouhamadou@Host-001.lan>
2024-12-09 20:20:34 +0100

    adding logo

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import { montserrat } from "../layout";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -5,7 +9,12 @@ export default function DashboardLayout({
   }>) {
     return <div className="h-screen flex">
         {/*LEFT */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">l</div>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+          <Link href="/" className="flex items-center justify-center gap-1">
+            <Image className="object-contain" src="/logog.png" alt="logo" width={32} height={32}/>
+            <span className={`${montserrat.className} hidden lg:block font-medium`}>GSchool</span>
+          </Link>
+          </div>
         {/*RIGHT */}
         <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
     </div>;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+export const montserrat = Montserrat({weight:["400", "500"], subsets:["latin"]});
 
 export const metadata: Metadata = {
   title: "Gael School Management Dashboard",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
