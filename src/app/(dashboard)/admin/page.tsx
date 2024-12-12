386a9ece2445b4a7008e8c16ae9aa8f56cf52ae7
Mouhamadou SY <mouhamadou@Host-001.lan>
2024-12-12 19:27:49 +0100

    Admin dashboard calendar implementation

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceCharts from "@/app/components/AttendanceCharts"
 import CountChart from "@/app/components/CountChart"
+import EventCalendar from "@/app/components/EventCalendar"
 import FinanceChart from "@/app/components/FinanceChart"
 import UserCard from "@/app/components/UserCard"
 
@@ -7,7 +8,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
-      <div className="w-full lg:w-2/3 flex flex-col gap-8">l
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARD */}
           <div className="flex gap-4 justify-between flex-wrap">
             <UserCard type="Student"/>
@@ -32,7 +33,9 @@ const AdminPage = () => {
         </div>
       </div>
       {/* Right */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+      </div>
     </div>
   )
 }
